357958ef17bbcf81a2690bed1e5f6070c7ebd688
BusinessAzouagh <business.azouagh@gmail.com>
2025-05-11 18:31:22 +0200

    deploy 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/portu-rent-wheels-go/",//url2
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: "/portu-rent-wheels-go/",
+  plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "src") },
   },
-}));
+});
