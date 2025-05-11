282e5a647f0481e1ee3145239127d9aba0eae400
BusinessAzouagh <business.azouagh@gmail.com>
2025-05-11 17:04:28 +0200

    deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portu-rent-wheels-go/",
+  base: "/portu-rent-wheels-go/",//url
   server: {
     host: "::",
     port: 8080,
